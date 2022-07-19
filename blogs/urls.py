759fab81eef619d6c2a1f679e364bbb38c0c99b1
Maksud <salokhiddinovmaksud@gmail.com>
2022-07-19 13:29:26 +0500

    ...

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from blogs.views import PostListView
+from blogs.views import PostListView, PostDetailView
 
 app_name = 'blogs'
 urlpatterns = [
-    path('', PostListView.as_view(), name='posts')
+    path('', PostListView.as_view(), name='posts'),
+    path('<int:pk>/post/', PostDetailView.as_view(), name='detail')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -5,8 +5,14 @@ from blogs.models import PostModel
 
 
 class PostListView(ListView):
-    queryset = PostModel.objects.order_by('-id')
     template_name = 'blog.html'
+
+    def get_queryset(self):
+        qs = PostModel.objects.order_by('-id')
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tag__name=tag)
+        return qs
 
 
 class PostDetailView(DetailView):
